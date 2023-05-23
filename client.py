018b0a007845f72a18c132357500a70f6ff77143
Egor Budanov <tujhrf2003@gmail.com>
2023-05-23 20:35:48 +0300

    ds

--- client.py
+++ client.py
@@ -5,13 +5,19 @@ def read_sok():
         data = sor.recv(1024)
         print(data.decode('utf-8'))
 server = ('127.0.0.4', 5000)# Данные сервера
-alias = input() # Вводим наш псевдоним
-print('s')
 sor = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sor.bind(('', 0)) # Задаем сокет как клиент
-sor.sendto((alias+' Connect to server').encode('utf-8'), server)# Уведомляем сервер о подключении
+sor.connect(server)
+alias = input() # Вводим наш псевдоним
+
+
+
+#sor.bind(('127.0.0.10', 0)) # Задаем сокет как клиент
+
+sor.sendall(alias.encode())# Уведомляем сервер о подключении
+print('s')
 potok = threading.Thread(target= read_sok)
+
 potok.start()
 while 1 :
     mensahe = input()
-    sor.sendto(('['+alias+']'+mensahe).encode('utf-8'), server)
+    sor.sendall(mensahe.encode())#
